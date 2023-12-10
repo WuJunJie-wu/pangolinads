54305e87ae49dbb521fd3d4ac5d0646c9aa9e50d
wjj <3181821287@qq.com>
2023-12-10 12:10:47 +0800

    2

--- app/src/main/java/com/chen/pangolinads/MainActivity.kt
+++ app/src/main/java/com/chen/pangolinads/MainActivity.kt
@@ -2,6 +2,7 @@ package com.chen.pangolinads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yuanli.ad.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
